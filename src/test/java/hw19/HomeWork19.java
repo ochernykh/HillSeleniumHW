4b74820124f9c16818b51b78f3a63916733c45f2
Oleh <0595887@gmail.com>
2020-06-02 18:32:54 +0300

    added homework 19

--- src/test/java/hw19/HomeWork19.java
+++ src/test/java/hw19/HomeWork19.java
@@ -1,4 +1,4 @@
-package hw4;
+package hw19;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
@@ -13,11 +13,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
-
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class HomeWork18 {
+public class HomeWork19 {
 
     public static String browser = "chrome";
     private WebDriver driver;
@@ -42,24 +41,33 @@ public class HomeWork18 {
     }
 
     @Test (description = "some basic test activity with sorting and checkboxes, including waiters and xpath search")
-    public void checkCoursesSortingButton(){
-        SoftAssert softAssert = new SoftAssert();
+    public void findUsedGermanBuses(){
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://udemy.com/ru/");
+        driver.get("https://auto.ria.com/");
         driver.manage().window().maximize();
-        driver.findElement(By.id("header-search-field")).sendKeys("java ");
-        driver.findElement(By.className("input-group-btn")).submit();
-        Select select = new Select(driver.findElement(By.xpath("//select[@name='sort']")));
-        select.selectByValue("most-reviewed");
-        softAssert.assertEquals(select.getFirstSelectedOption().getText(),
-                "Больше всего отзывов" , "check if courses list is sorted by most reviewed");
-        waiter.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@value='beginner' and @type='checkbox']/parent::label"))).click();
-        waiter.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[@class='js-panel-toggler  panel--label--qoWJs' and contains(text(),'Язык')]"))).click();
-        waiter.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@value='en' and @type='checkbox']/parent::label"))).click();
-        WebElement checkBox = driver.findElement(By.xpath("//input[@value='en' and @type='checkbox']"));
-        softAssert.assertEquals(checkBox.isEnabled(),
-                true , "check if english speaking courses checkbox is enabled");
-        softAssert.assertAll();
+        driver.findElement(By.xpath("//a[@class='ext-end']/child::span")).click();
+        driver.findElement(By.xpath("//label[@class='c-notifier-btn']")).click();
+        driver.findElement(By.xpath("//label[@for='custom-used']")).click();
+        Select transportType = new Select(driver.findElement(By.xpath("//select[@aria-label='Тип транспорта']")));
+        transportType.selectByValue("7");
+        List<WebElement> carTypes = driver.findElements(By.xpath("//label[starts-with(@for, \"body.id\")]"));
+        for (WebElement el: carTypes) {
+            el.click();
+        }
+        Select country = new Select(driver.findElement(By.id("at_country")));
+        country.selectByVisibleText("Германия");
+        waiter.until(ExpectedConditions.elementToBeClickable(By.id("autocompleteInput-brand-0"))).sendKeys("Volkswagen");
+        WebElement carMakeUL = driver.findElement(By.xpath("//div[@id='autocomplete-brand-0']/ul"));
+        List<WebElement> carMakes = carMakeUL.findElements(By.tagName("li"));
+        for (WebElement li: carMakes) {
+            if(li.getText().equals("Volkswagen")){
+                li.submit();
+            }
+
+        }
+        driver.findElement(By.id("at_price-from")).sendKeys("5000");
+        driver.findElement(By.id("at_price-to")).sendKeys("10000");
+        driver.findElement(By.xpath("//button[@class='button small']")).click();
     }
 
     @AfterClass
